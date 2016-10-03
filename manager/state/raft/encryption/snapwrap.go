423492f82c5c61f0e5623ff3b9a1e1ad33c79720
cyli <ying.li@docker.com>
2016-10-03 16:06:44 -0700

    Implement snapshot encoding and decoding
    Signed-off-by: cyli <ying.li@docker.com>

---
+++ manager/state/raft/encryption/snapwrap.go
@@ -0,0 +1,121 @@
+package encryption
+
+import (
+	"fmt"
+
+	"github.com/coreos/etcd/raft/raftpb"
+	"github.com/coreos/etcd/snap"
+)
+
+// This package wraps the github.com/coreos/etcd/snap package, and encodes
+// the bytes of whatever snapshot is passed to it, and decodes the bytes of
+// whatever snapshot it reads.
+
+// Snapshotter is the interface presented by github.com/coreos/etcd/snap.Snapshotter
+type Snapshotter interface {
+	SaveSnap(snapshot raftpb.Snapshot) error
+	Load() (*raftpb.Snapshot, error)
+}
+
+// Make sure WAL implements the same interface as wal.WAL
+var _ Snapshotter = &WrappedSnap{}
+var _ Snapshotter = &snap.Snapshotter{}
+
+// WrappedSnap wraps a github.com/coreos/etcd/snap.Snapshotter, and handles
+// encoding/decoding.
+type WrappedSnap struct {
+	*snap.Snapshotter
+	encoder  encoder
+	decoders map[string]decoder
+}
+
+// NewSnapshotter returns a new Snapshotter with the given encoders and decoders
+func NewSnapshotter(dirpath string, e encoder, decoders []decoder) Snapshotter {
+	mappedDecoders := make(map[string]decoder)
+	for _, dec := range decoders {
+		mappedDecoders[dec.ID()] = dec
+	}
+
+	return &WrappedSnap{
+		Snapshotter: snap.New(dirpath),
+		encoder:     e,
+		decoders:    mappedDecoders,
+	}
+}
+
+// SaveSnap encodes the snapshot data (if an encoder is exists) before passing it onto the
+// wrapped snap.Snapshotter's SaveSnap function.
+func (s *WrappedSnap) SaveSnap(snapshot raftpb.Snapshot) error {
+	toWrite := snapshot
+	if s.encoder != nil {
+		var err error
+		wrapped := WrappedRecord{
+			Encoding: s.encoder.ID(),
+		}
+		wrapped.Wrapped, err = s.encoder.Encode(snapshot.Metadata.Index, snapshot.Metadata.Term, snapshot.Data)
+		if err != nil {
+			return fmt.Errorf("unable to encode entry data: %s", err.Error())
+		}
+		toWrite.Data, err = wrapped.Marshal()
+		if err != nil {
+			return err
+		}
+	}
+	return s.Snapshotter.SaveSnap(toWrite)
+}
+
+// Load decodes the snapshot data (if a decoder is exists) after reading it using the
+// wrapped snap.Snapshotter's Load function.
+func (s *WrappedSnap) Load() (*raftpb.Snapshot, error) {
+	snapshot, err := s.Snapshotter.Load()
+	if err != nil {
+		return nil, err
+	}
+	if err := decodeSnaphot(snapshot, s.decoders); err != nil {
+		return nil, err
+	}
+	return snapshot, nil
+}
+
+// ReadSnap reads the snapshot named by snapname using the given decoders and returns the snapshot.
+func ReadSnap(snapname string, decoders []decoder) (*raftpb.Snapshot, error) {
+	snapshot, err := snap.Read(snapname)
+	if err != nil {
+		return nil, err
+	}
+
+	mappedDecoders := make(map[string]decoder)
+	for _, dec := range decoders {
+		mappedDecoders[dec.ID()] = dec
+	}
+
+	if err := decodeSnaphot(snapshot, mappedDecoders); err != nil {
+		return nil, err
+	}
+	return snapshot, nil
+}
+
+func decodeSnaphot(snapshot *raftpb.Snapshot, decoders map[string]decoder) error {
+	wrappedRecord := WrappedRecord{}
+	err := wrappedRecord.Unmarshal(snapshot.Data) // nope, this wasn't marshalled as a WrappedRecord
+	if err != nil {
+		return nil
+	}
+
+	if wrappedRecord.Encoding == "" {
+		snapshot.Data = wrappedRecord.Wrapped
+		return nil
+	}
+
+	d, ok := decoders[wrappedRecord.Encoding]
+	if !ok || d == nil {
+		return fmt.Errorf("no decoder available for %s", wrappedRecord.Encoding)
+	}
+
+	snapshot.Data, err = d.Decode(snapshot.Metadata.Index, snapshot.Metadata.Term, wrappedRecord.Wrapped)
+	if err != nil {
+		return fmt.Errorf("unable to decode snapshot: %s", err.Error())
+	}
+
+	return nil
+}
---
+++ manager/state/raft/encryption/snapwrap_test.go
@@ -0,0 +1,223 @@
+package encryption
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/coreos/etcd/raft/raftpb"
+	"github.com/coreos/etcd/snap"
+	"github.com/stretchr/testify/require"
+)
+
+var fakeSnapshotData = raftpb.Snapshot{
+	Data: []byte("snapshotdata"),
+	Metadata: raftpb.SnapshotMetadata{
+		ConfState: raftpb.ConfState{Nodes: []uint64{3}},
+		Index:     6,
+		Term:      2,
+	},
+}
+
+func getSnapshotFile(t *testing.T, tempdir string) string {
+	var filepaths []string
+	err := filepath.Walk(tempdir, func(path string, fi os.FileInfo, err error) error {
+		require.NoError(t, err)
+		if !fi.IsDir() {
+			filepaths = append(filepaths, path)
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	require.Len(t, filepaths, 1)
+	return filepaths[0]
+}
+
+// Snapshotter can read snapshots written by a regular snap.Snapshot
+func TestSnapshotterLoadOrReadUnencodedSnapshot(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	ogSnap := snap.New(tempdir)
+	require.NoError(t, ogSnap.SaveSnap(fakeSnapshotData))
+
+	decoders := []decoder{&meowCoder{}}
+	wrapped := NewSnapshotter(tempdir, nil, decoders)
+
+	readSnap, err := wrapped.Load()
+	require.NoError(t, err)
+	require.Equal(t, fakeSnapshotData, *readSnap)
+
+	readSnap, err = ReadSnap(getSnapshotFile(t, tempdir), decoders)
+	require.NoError(t, err)
+	require.Equal(t, fakeSnapshotData, *readSnap)
+}
+
+// Snapshotter can read snapshots produced with an empty encoding
+func TestSnapshotterLoadEmptyEncodingSnapshot(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	ogSnap := snap.New(tempdir)
+	wr := WrappedRecord{
+		Wrapped: fakeSnapshotData.Data,
+	}
+	wrData, err := wr.Marshal()
+	require.NoError(t, err)
+
+	emptyEncodingFakeData := fakeSnapshotData
+	emptyEncodingFakeData.Data = wrData
+
+	require.NoError(t, ogSnap.SaveSnap(emptyEncodingFakeData))
+
+	decoders := []decoder{&meowCoder{}}
+	wrapped := NewSnapshotter(tempdir, nil, decoders)
+
+	readSnap, err := wrapped.Load()
+	require.NoError(t, err)
+	require.Equal(t, fakeSnapshotData, *readSnap)
+
+	readSnap, err = ReadSnap(getSnapshotFile(t, tempdir), decoders)
+	require.NoError(t, err)
+	require.Equal(t, fakeSnapshotData, *readSnap)
+}
+
+// If there is no decoder for a snapshot, decoding fails
+func TestSnapshotterLoadNoDecoder(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	coder := &meowCoder{}
+
+	ogSnap := snap.New(tempdir)
+	wr := WrappedRecord{
+		Wrapped:  fakeSnapshotData.Data,
+		Encoding: coder.ID(),
+	}
+	wrData, err := wr.Marshal()
+	require.NoError(t, err)
+
+	emptyEncodingFakeData := fakeSnapshotData
+	emptyEncodingFakeData.Data = wrData
+
+	require.NoError(t, ogSnap.SaveSnap(emptyEncodingFakeData))
+
+	wrapped := NewSnapshotter(tempdir, nil, nil)
+
+	_, err = wrapped.Load()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no decoder available")
+
+	_, err = ReadSnap(getSnapshotFile(t, tempdir), nil)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no decoder available")
+}
+
+// If decoding a snapshot fails, the error is propagated
+func TestSnapshotterLoadDecodingFail(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	coder := &meowCoder{}
+
+	ogSnap := snap.New(tempdir)
+	wr := WrappedRecord{
+		Wrapped:  fakeSnapshotData.Data,
+		Encoding: coder.ID(),
+	}
+	wrData, err := wr.Marshal()
+	require.NoError(t, err)
+
+	emptyEncodingFakeData := fakeSnapshotData
+	emptyEncodingFakeData.Data = wrData
+
+	require.NoError(t, ogSnap.SaveSnap(emptyEncodingFakeData))
+
+	decoders := []decoder{&meowCoder{}}
+	wrapped := NewSnapshotter(tempdir, nil, decoders)
+
+	_, err = wrapped.Load()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unable to decode snapshot")
+
+	_, err = ReadSnap(getSnapshotFile(t, tempdir), decoders)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unable to decode snapshot")
+}
+
+// If no encoder is passed to Snapshotter, the resulting snapshot can be
+// read by the regular snap.Snapshotter
+func TestSnapshotterSavesSnapshotNoEncoding(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	wrapped := NewSnapshotter(tempdir, nil, nil)
+	require.NoError(t, wrapped.SaveSnap(fakeSnapshotData))
+
+	ogSnap := snap.New(tempdir)
+	readSnap, err := ogSnap.Load()
+	require.NoError(t, err)
+
+	require.Equal(t, fakeSnapshotData, *readSnap)
+}
+
+// If an encoder is passed to Snapshotter, the resulting snapshot data (but not
+// metadata or anything else) is encoded before being passed to the wrapped Snapshotter.
+func TestSnapshotterSavesSnapshotWithEncoding(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	wrapped := NewSnapshotter(tempdir, &meowCoder{}, nil)
+	require.NoError(t, wrapped.SaveSnap(fakeSnapshotData))
+
+	ogSnap := snap.New(tempdir)
+	readSnap, err := ogSnap.Load()
+	require.NoError(t, err)
+
+	require.Equal(t, fakeSnapshotData.Metadata, readSnap.Metadata)
+	require.NotEqual(t, fakeSnapshotData.Data, readSnap.Data)
+}
+
+// If an encoder is passed to Snapshotter, but encoding the data fails, the
+// error is propagated up
+func TestSnapshotterSavesSnapshotEncodingFails(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "snapwrap")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempdir)
+
+	wrapped := NewSnapshotter(tempdir, &meowCoder{encodeFailures: map[string]struct{}{
+		fmt.Sprintf("%d_%d", fakeSnapshotData.Metadata.Index, fakeSnapshotData.Metadata.Term): struct{}{},
+	}}, nil)
+	err = wrapped.SaveSnap(fakeSnapshotData)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "refusing to encode")
+
+	// nothing there to read
+	ogSnap := snap.New(tempdir)
+	_, err = ogSnap.Load()
+	require.Error(t, err)
+}
+
+// Snapshotter can read what it wrote so long as it has the same decoder
+func TestSaveAndLoad(t *testing.T) {
+	coder := &meowCoder{}
+	for _, e := range []encoder{nil, coder} {
+		tempdir, err := ioutil.TempDir("", "waltests")
+		require.NoError(t, err)
+		defer os.RemoveAll(tempdir)
+
+		wrapped := NewSnapshotter(tempdir, e, []decoder{coder})
+		require.NoError(t, wrapped.SaveSnap(fakeSnapshotData))
+		readSnap, err := wrapped.Load()
+		require.NoError(t, err)
+		require.Equal(t, fakeSnapshotData, *readSnap)
+	}
+}
