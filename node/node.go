13c2209e4f1b3984a5e48a767ea13e9acd956211
Aaron Lehmann <aaron.lehmann@docker.com>
2017-05-23 12:07:55 -0700

    Act on AddrMismatch flag by rejoining, if advertise address was autodetected
    A new flag is provided that tells the node whether the advertise address
    was automatically detected. If it was, and the node's address changed
    from the perspective of another node, it will attempt to call Join again
    to update its address in Raft.
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- node/node.go
+++ node/node.go
@@ -98,6 +98,11 @@ type Config struct {
 	// for connections to the remote API (including the raft service).
 	AdvertiseRemoteAPI string
 
+	// AdvertiseAutodetected specifies that the advertise address was
+	// autodetected, and therefore should be updated automatically if it
+	// changes from the perspective of other nodes.
+	AdvertiseAutodetected bool
+
 	// Executor specifies the executor to use for the agent.
 	Executor exec.Executor
 
@@ -219,7 +224,7 @@ func New(c *Config) (*Node, error) {
 }
 
 // BindRemote starts a listener that exposes the remote API.
-func (n *Node) BindRemote(ctx context.Context, listenAddr string, advertiseAddr string) error {
+func (n *Node) BindRemote(ctx context.Context, listenAddr string, advertiseAddr string, advertiseAutodetected bool) error {
 	n.RLock()
 	defer n.RUnlock()
 
@@ -228,8 +233,9 @@ func (n *Node) BindRemote(ctx context.Context, listenAddr string, advertiseAddr 
 	}
 
 	return n.manager.BindRemote(ctx, manager.RemoteAddrs{
-		ListenAddr:    listenAddr,
-		AdvertiseAddr: advertiseAddr,
+		ListenAddr:            listenAddr,
+		AdvertiseAddr:         advertiseAddr,
+		AdvertiseAutodetected: advertiseAutodetected,
 	})
 }
 
@@ -814,8 +820,9 @@ func (n *Node) runManager(ctx context.Context, securityConfig *ca.SecurityConfig
 	var remoteAPI *manager.RemoteAddrs
 	if n.config.ListenRemoteAPI != "" {
 		remoteAPI = &manager.RemoteAddrs{
-			ListenAddr:    n.config.ListenRemoteAPI,
-			AdvertiseAddr: n.config.AdvertiseRemoteAPI,
+			ListenAddr:            n.config.ListenRemoteAPI,
+			AdvertiseAddr:         n.config.AdvertiseRemoteAPI,
+			AdvertiseAutodetected: n.config.AdvertiseAutodetected,
 		}
 	}
 
